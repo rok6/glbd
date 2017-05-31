b51a027debeb6facbec2590f7230c5f1b0ed391d
rok6 <sechs06re.ryo+glbd@gmail.com>
2017-05-31 14:48:00 +0900

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>GLBD</title>
+
+	<meta property="og:site_name" content="サイト名" />
+	<meta property="og:type" content="ページの種類" />
+	<meta property="og:url" content="ページのURL" />
+	<meta property="og:image" content="サムネイル画像のURL" />
+
+	<meta name="twitter:site" content="@gelubde">
+	<meta name="twitter:card" content="Twitterカードの種類" />
+
+	<link rel="stylesheet" href="assets/css/ress.css" />
+	<link rel="stylesheet" href="assets/css/style.css" />
+
+</head>
+<body>
+
+	<header id="header">
+		<div class="header-logo">
+			<a href="#site_url">
+				<h1>GLBD</h1>
+			</a>
+		</div>
+		<nav id="navigation">
+			<ul>
+				<li><a href="#menu_url">グローバルメニュー</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<div role="main" id="main">
+
+		<section class="select-content main-section">
+			<div class="container">
+				<nav class="select-content-nav">
+					<div class="select-content-nav-menu"><a href="#archives"><h2>Archive - [R66]</h2></a></div>
+					<div class="select-content-nav-menu"><a href="#projects"><h2>Projects</h2></a></div>
+				</nav>
+			</div>
+		</section>
+
+	</div>
+
+	<footer id="footer">
+		<div class="copyright">&copy; 2017 glbd.</div>
+	</footer>
+
+	<!-- Load Scripts -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+		integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+		crossorigin="anonymous">
+	</script>
+	<script src="assets/js/index.js"></script>
+	<!--// Load Scripts -->
+
+</body>
+</html>
